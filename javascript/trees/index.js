4a70b675d4d0023274d1d2894e2f6761bcc1090d
Hasnaa Al-Habahbeh <hasnaahab98@gmail.com>
2021-11-23 15:37:14 +0200

    ch18

--- javascript/trees/index.js
+++ javascript/trees/index.js
@@ -1,8 +1,6 @@
 'use strict';
 
-let Node = require('./lib/Node');
-let BinaryTree = require('./lib/BinaryTree');
-let BST = require('./lib/BinarySearchTree');
+let {Node, KaryTree, fizzBuzz} = require('./lib/KaryTree');
 
 // function doWork(){
 //   let node1 = new Node(50);
@@ -29,30 +27,17 @@ let BST = require('./lib/BinarySearchTree');
 // }
 
 function doWork(){
-  // let newTree = new BST();
-  // newTree.add(15);
-  // newTree.add(3);
-  // newTree.add(36);
-  // newTree.add(12);
-  // newTree.add(2);
-  // newTree.add(28);
-  // newTree.add(39);
-  // // console.log(newTree);
-  // console.log(newTree.max());
-  // console.log(newTree.min());
+  let node7 = new Node(15);
+  let node6 = new Node(6);
+  let node5 = new Node(5);
+  let node4 = new Node(4);
+  let node3 = new Node(30);
+  let node2 = new Node(2, [node6, node7]);
+  let node1 = new Node(1, [node2, node3, node4, node5]);
+  let myTree = new KaryTree(node1);
+  // console.log(myTree);
 
-  let newBT = new BinaryTree();
-  let node10 = new Node(5);
-  let node11 = new Node(11);
-  let node4 = new Node(2);
-  let node5 = new Node(6, node10, node11);
-  let node8 = new Node(4);
-  let node7 = new Node(9, node8, null);
-  let node2 = new Node(7, node4, node5);
-  let node3 = new Node(5, null, node7);
-  newBT.root = new Node(2, node2, node3);
-  // console.log(newBT.preOrder());
-  console.log(newBT.breadth());
+  console.log(fizzBuzz(myTree));
 }
 
 module.exports = doWork;
